b839256732031f668d6264d689f83049d0eeb130
Devesh Pathak <deveshpathak1104@gmail.com>
2026-06-12 00:32:58 +0530

    Connect backend to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require("./config/db");
 
 const app = express();
 const port= process.env.PORT;
@@ -14,6 +15,8 @@ app.get('/api/health',(req,res) =>{
         message: "Server is running"
     });
 });
+
+connectDB();
 
 app.listen(port,(req,res) =>{
     console.log(`Server started at port: ${port}`);
