7b619e23d30d9de41a22b88b6d94bdaec277e813
Devesh Pathak <deveshpathak1104@gmail.com>
2026-06-12 19:34:43 +0530

    Implement user registratin and password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,40 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 
 const registerUser = async (req,res) => {
-    res.json({
-        message: "register route warking"
-    });
+    try{
+        const {name,email,password}= req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword= await bcrypt.hash(password,10);
+
+        const user = await User.create({
+            name,
+            email,
+            hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user
+        });
+
+
+    }
+    catch(error){
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
+        });
+    }
 };
 
 module.exports = { registerUser };
--- server/models/User.js
+++ server/models/User.js
@@ -8,16 +8,16 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    password:{
-        type: String,
-        required: true,
-        minlength: 8
-    },
     email:{
         type: String,
         required: true,
         unique: true,
         lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8
     },
     role:{
         type: String,
